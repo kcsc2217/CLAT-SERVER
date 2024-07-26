cfa0638e47bb88a1d7a72ec58659de6fcd4c5f62
akk <rudrhs9309@naver.com>
2024-07-26 23:08:10 +0900

    COMMIT

---
+++ src/main/java/team_project/clat/ClatApplication.java
@@ -0,0 +1,13 @@
+package team_project.clat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team_project/clat/ClatApplicationTests.java
@@ -0,0 +1,13 @@
+package team_project.clat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
