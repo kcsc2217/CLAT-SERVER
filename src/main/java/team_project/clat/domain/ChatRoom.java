677e7564fd8d01c80eb417d67640106c3d02ac58
akk <rudrhs9309@naver.com>
2024-07-27 17:48:32 +0900

    Add Entity Mapping

--- src/main/java/team_project/clat/domain/ChatRoom.java
+++ src/main/java/team_project/clat/domain/ChatRoom.java
@@ -1,4 +1,30 @@
 package team_project.clat.domain;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ChatRoom {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CHARROOM_ID")
+    private Long id;
+
+    private String roomName;
+
+    @OneToMany(mappedBy = "chatroom", cascade = CascadeType.ALL)
+    List<Message> messageList = new ArrayList<>();
+
+    public ChatRoom(Long id, String roomName) {
+        this.id = id;
+        this.roomName = roomName;
+    }
 }
--- src/main/java/team_project/clat/domain/Memo.java
+++ src/main/java/team_project/clat/domain/Memo.java
@@ -1,4 +1,35 @@
 package team_project.clat.domain;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Memo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "MEMO_ID")
+    private Long id;
+
+    private String Memo;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="MESSAGE_ID")
+    private Message message;
+
+
+    public void addMessage(Message message){
+        this.message=message;
+        message.getMemoList().add(this);
+    }
+
+    public Memo(Long id, String memo, Message message) {
+        this.id = id;
+        Memo = memo;
+        this.message = message;
+    }
 }
