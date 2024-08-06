827ebe0ab73e0d31bc7ed71ccdf00b0fac524fcd
akk <rudrhs9309@naver.com>
2024-08-07 01:12:51 +0900

    Update Controller

--- src/main/java/team_project/clat/controller/AdminController.java
+++ src/main/java/team_project/clat/controller/AdminController.java
@@ -2,9 +2,11 @@ package team_project.clat.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
+@ResponseBody
 public class AdminController {
 
     @GetMapping("/admin")
--- src/main/java/team_project/clat/controller/MainController.java
+++ src/main/java/team_project/clat/controller/MainController.java
@@ -1,9 +1,12 @@
 package team_project.clat.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
+@ResponseBody
 public class MainController {
 
     @GetMapping("/")
