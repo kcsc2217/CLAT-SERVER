a98cf7c150d7f445654b8182bf89f381456787c0
kcsc2217 <k12002@nate.com>
2024-08-26 16:51:39 +0900

    student_course data inject

---
+++ src/main/java/team_project/clat/repository/Student_course_Repository.java
@@ -0,0 +1,7 @@
+package team_project.clat.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import team_project.clat.domain.Student_course;
+
+public interface Student_course_Repository extends JpaRepository<Student_course, Long> {
+}
